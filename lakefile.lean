78d82178b9c37201c3f37a90ace2193bb77f7419
Chris A. Upshaw <chrisaupshaw@gmail.com>
2022-04-07 14:49:13 -0700

    Update of lakefile and toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,8 @@
 import Lake
-open Lake DSL
+open System Lake DSL
 
 package pharoh {
-
+dependencies := #[
+    { name := `mathlib, src := Source.path (FilePath.mk "/home" / "nolrai" / "Lean4" / "mathlib4") }
+   ]
 }
